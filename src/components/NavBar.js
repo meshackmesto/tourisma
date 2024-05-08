75f6388150329de146e1c5d84aa567b0e12410c5
meshackmesto <meshackorina93@gmail.com>
2024-05-08 15:30:18 +0300

    done

---
+++ src/components/NavBar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { NavLink } from'react-router-dom';
+import "./NavBar.css";
+import logo from './logo_image/logo.png';
+
+
+
+function NavBar(){
+    return (
+        <div className='navbar'>
+        <nav>
+            <img className='logo'
+            src={logo} alt='Tourisma logo'/>
+            <NavLink 
+            to="/"
+            className="nav-link"
+            >
+                Home
+            </NavLink>
+            <NavLink 
+            to="/login"
+            className="nav-link"
+            >
+                Login
+            </NavLink>
+            <NavLink 
+            to= "/sign-up"
+            className="nav-link"
+            >
+                sign-up
+            </NavLink>
+            <NavLink 
+            to= "/favourite"
+            className="nav-link"
+            >
+                favourite
+            </NavLink>
+            <NavLink 
+            to= "/visit"
+            className="nav-link"
+            >
+                visit
+            </NavLink>
+        </nav>
+        <div className='searchBox'> 
+        <input type='text' placeholder='Search'></input>
+        </div>
+        </div>
+    );
+};
+
+export default NavBar;
