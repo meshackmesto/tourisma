395d34042a23df90d3b3715025c7152e7321d9a7
Charles <charlesmuteithia@gmail.com>
2024-05-09 09:42:08 +0300

    tourisma

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,7 @@
 import React from'react';
 import './App.css';
 import NavBar from '../components/NavBar';
-import CityList from '../src/components/CityList'
+import CityList from '../components/CityList';
 
 function App() {
   return (
