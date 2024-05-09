2ca8c4ad28cfdc164003ffdecf5d2a1fd4ba1c80
Charles <charlesmuteithia@gmail.com>
2024-05-09 10:02:34 +0300

    removes the main error located at app.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,8 @@
 import React from'react';
 import './App.css';
 import NavBar from '../components/NavBar';
-main
 import CityList from '../components/CityList';
 import TravelForm from './TravelForm';
-main
 
 function App() {
   return (
